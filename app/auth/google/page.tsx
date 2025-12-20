12d41eea51dcd33c9032934430fdc422e4473128
ArielSt95V2 <astablediffusion2@gmail.com>
2025-12-20 09:36:11 +0200

    Fix: Wrap useSearchParams() in Suspense boundary for Facebook and Google auth pages

--- app/auth/google/page.tsx
+++ app/auth/google/page.tsx
@@ -1,10 +1,11 @@
 'use client';
 
+import { Suspense } from 'react';
 import { useSocialAuthenticateMutation } from '@/redux/features/authApiSlice';
 import { useSocialAuth } from '@/hooks';
 import { Spinner } from '@/components/common';
 
-export default function Page() {
+function GoogleAuthContent() {
 	const [googleAuthenticate] = useSocialAuthenticateMutation();
 	useSocialAuth(googleAuthenticate, 'google-oauth2');
 
@@ -12,5 +13,17 @@ export default function Page() {
 		<div className='my-8'>
 			<Spinner lg />
 		</div>
+	);
+}
+
+export default function Page() {
+	return (
+		<Suspense fallback={
+			<div className='my-8'>
+				<Spinner lg />
+			</div>
+		}>
+			<GoogleAuthContent />
+		</Suspense>
 	);
 }
