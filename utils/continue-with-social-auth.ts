f62ce1579f420748c2b35bb47ce6c788295b0d39
ArielSt95V2 <astablediffusion2@gmail.com>
2025-12-27 08:37:49 +0200

    Request Tracing (End-to-End)

--- utils/continue-with-social-auth.ts
+++ utils/continue-with-social-auth.ts
@@ -10,7 +10,7 @@ export default async function continueWithSocialAuth(
 		}/api/o/${provider}/?redirect_uri=${
 			process.env.NODE_ENV === 'production'
 				? process.env.NEXT_PUBLIC_REDIRECT_URL
-				: `https://${process.env.NEXT_PUBLIC_DOMAIN || 'localhost:3000'}`  // ✅ Add http:// prefix
+				: `https://${process.env.NEXT_PUBLIC_DOMAIN || 'localhost'}`  // ✅ Add http:// prefix
 		}/auth/${redirect}`;
 
 		const res = await fetch(url, {
